4b58de9515b3d1961365fd55ac5f4302d1032db9
Redha Bayu Anggara <redhabayuanggara@gmail.com>
2023-06-17 14:49:41 +0700

    initialize

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,65 @@
+class Api::V1::ArticlesController < ApplicationController
+
+  def index
+    articles = Article.all
+    render json: articles, status: 200
+  end
+
+  def show
+    article = Article.find_by(id: params[:id])
+    if article
+        render json: article, status: 200
+    else
+      render json: {
+        error: "Article Not Found.." 
+      }
+    end
+  end
+
+  def create
+    article = Article.new(
+      title: arti_params[:title],
+      body: arti_params[:body],
+      author: arti_params[:author],
+    )
+    if article.save
+      render json: article, status: 200
+    else
+      render json: {
+        error: "Error Creating.."
+      }
+    end
+  end
+
+  def update
+    article = Article.find_by(id: params[:id])
+    if article 
+        article.update(
+          title: params[:title],
+          body: params[:body],
+          author: params[:author]
+        )
+      render json: "Artcles Record Updated Succesfully"
+    else
+      render json: {
+        error: "Artcles Not Found"
+      }
+    end
+  end
+
+  def destroy
+    article = Article.find_by(id: params[:id])
+    if article 
+      article.destroy
+      render json: "Artcles Record Updated Succesfully"
+    end
+  end
+
+  private
+  def arti_params
+      params.required(:article).permint([
+        :title,
+        :body,
+        :author
+      ])
+end
